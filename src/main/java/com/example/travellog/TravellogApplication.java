86cec847a38e557e6fe768ac3690c47722444ebb
Jake6654 <ugw2ad@virginia.edu>
2025-03-29 12:35:01 -0400

    "Initial project commit"

---
+++ src/main/java/com/example/travellog/TravellogApplication.java
@@ -0,0 +1,13 @@
+package com.example.travellog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellogApplication.class, args);
+	}
+
+}
