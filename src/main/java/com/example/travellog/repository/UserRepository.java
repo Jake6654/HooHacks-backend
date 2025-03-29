30b69e0580e0938b5763eb3ddec82bd9f365bba6
Jake6654 <ugw2ad@virginia.edu>
2025-03-29 15:04:03 -0400

    feat: Integrate Google OAuth2 login with JWT issuance after authentication - Implemented CustomOAuth2UserService to extract user info from Google - Issued JWT upon successful OAuth2 login and stored it in HttpOnly cookie - Configured Spring Security to support stateless JWT + OAuth2 login - Updated UserRepository and MongoDB model to store new users

---
+++ src/main/java/com/example/travellog/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.travellog.repository;
+
+import com.example.travellog.document.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String > {
+    Optional<User> findByEmail(String email); // 이메일로 유저 검색
+}
